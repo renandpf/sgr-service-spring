923b74af745545bd77858c5976f06d770d009df8
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-07-14 17:44:47 -0300

    fix: adicionando 'produto'

--- src/main/java/br/com/pupposoft/fiap/sgr/gerencial/cliente/adapter/driver/controller/ClienteController.java
+++ src/main/java/br/com/pupposoft/fiap/sgr/gerencial/cliente/adapter/driver/controller/ClienteController.java
@@ -1,5 +1,62 @@
 package br.com.pupposoft.fiap.sgr.gerencial.cliente.adapter.driver.controller;
 
+import br.com.pupposoft.fiap.sgr.gerencial.cliente.adapter.driver.controller.json.ClienteJson;
+import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.application.usecase.AlterarClienteUsecase;
+import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.application.usecase.CriarClienteUsecase;
+import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.application.usecase.ObterClienteUsecase;
+import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.dto.ClienteDto;
+import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.dto.flows.AlterarClienteParamsDto;
+import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.dto.flows.CriarClienteParamsDto;
+import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.dto.flows.CriarClienteReturnDto;
+import lombok.extern.slf4j.Slf4j;
+
+//@Controller("/clientes")
+@Slf4j
 public class ClienteController {
 
+	private ObterClienteUsecase obterClienteUseCase;
+	private CriarClienteUsecase criarClienteUseCase;
+	private AlterarClienteUsecase alterarClienteUseCase;
+
+	//	  @Get("/cpf/:cpf")
+	//	  @Returns(200, ClienteJson)
+	//	  @Returns(404).Description("Not found")
+	public ClienteJson obterPorCpf(/*@PathParams("cpf")*/ String cpf) {
+		log.trace("Start cpf={}", cpf);
+		ClienteDto clienteDto = this.obterClienteUseCase.obterPorCpf(cpf);
+		ClienteJson clienteJson = new ClienteJson(clienteDto);
+		log.trace("End clienteJson={}", clienteJson);
+		return clienteJson;
+	}
+
+	//	  @Get("/email/:email")
+	//	  @Returns(200, ClienteJson)
+	//	  @Returns(404).Description("Not found")
+	public ClienteJson obterPorEmail(/*@PathParams("email") */ String email) {
+		log.trace("Start email={}", email);
+		ClienteDto clienteDto = this.obterClienteUseCase.obterPorEmail(email);
+		ClienteJson clienteJson = new ClienteJson(clienteDto);
+		log.trace("End clienteJson={}", clienteJson);
+		return clienteJson;
+	}
+
+	//	  @Post("/")
+	//	  @Returns(201, ClienteJson)
+	//	  @Returns(404).Description("Not found")
+	public Long criarCliente(/*@BodyParams()*/ ClienteJson cliente) {
+		log.trace("Start cliente={}", cliente);
+		CriarClienteReturnDto returnDto = this.criarClienteUseCase.criar(CriarClienteParamsDto.builder().cliente(cliente.getDto()).build());
+		Long clienteId = returnDto.getClientId();
+		log.trace("End clienteId={}", clienteId);
+		return clienteId;
+	}
+
+	//	@Put("/:id")
+	//	@Returns(200)
+	//	@Returns(404).Description("Not found")
+	public void alterarCliente(/*@BodyParams() */ClienteJson clienteJson, /*@PathParams("id")*/ Long id){
+		log.trace("Start clienteJson={}, id={}", clienteJson, id);
+		this.alterarClienteUseCase.alterar(AlterarClienteParamsDto.builder().cliente(clienteJson.getDto(id)).build());
+		log.trace("End");
+	}
 }
--- src/main/java/br/com/pupposoft/fiap/sgr/gerencial/cliente/adapter/driver/controller/json/ClienteJson.java
+++ src/main/java/br/com/pupposoft/fiap/sgr/gerencial/cliente/adapter/driver/controller/json/ClienteJson.java
@@ -1,5 +1,6 @@
 package br.com.pupposoft.fiap.sgr.gerencial.cliente.adapter.driver.controller.json;
 
+import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.dto.ClienteDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -12,4 +13,29 @@ public class ClienteJson {
     private String nome;
     private String cpf;
     private String email;
+    
+    public ClienteJson(ClienteDto dto){
+        this.id = dto.getId();
+        this.nome = dto.getNome();
+        this.cpf = dto.getCpf();
+        this.email = dto.getEmail();
+    }
+
+    public ClienteDto getDto(Long id) {
+        return ClienteDto.builder()
+        		.id(id)
+        		.nome(nome)
+        		.cpf(cpf)
+        		.email(email)
+        		.build();
+    }
+    
+    public ClienteDto getDto() {
+    	return ClienteDto.builder()
+    			.id(id)
+    			.nome(nome)
+    			.cpf(cpf)
+    			.email(email)
+    			.build();
+    }
 }
