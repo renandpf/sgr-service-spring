a1ce6f1eadd5698d33e95479eda0a2106a016bdc
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-09-03 09:53:17 -0300

    Implementando Pedido.setStatus

--- src/main/java/br/com/pupposoft/fiap/sgr/pedido/core/domain/Pedido.java
+++ src/main/java/br/com/pupposoft/fiap/sgr/pedido/core/domain/Pedido.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.domain.Cliente;
 import br.com.pupposoft.fiap.sgr.pagamento.core.domain.Pagamento;
+import br.com.pupposoft.fiap.sgr.pedido.core.exception.AlteracaoStatusPedidoException;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -25,13 +26,64 @@ public class Pedido {
 
 	
     public boolean temCliente() {
-        return this.cliente != null;
+        return cliente != null;
     }
     
-	public void setStatus(Status status) {
-		//TODO: IMPLEMENTAR
+    //TODO: Aplicar SOLID: este método tende a crescer sempre q surgir novos status. 
+    //Sugestão: "status" deveria ser uma classe (domain) ao invés de enum
+	public void setStatus(Status newStatus) {
 		
-		this.status = status;
+        switch (newStatus) {
+        case RECEBIDO:
+            if (status == null || status.equals(Status.RECEBIDO)) {
+                status = newStatus;
+                return;
+            }
+            throw new AlteracaoStatusPedidoException();
+
+        case AGUARDANDO_CONFIRMACAO_PAGAMENTO:
+            if (status.equals(Status.RECEBIDO)) {
+                status = newStatus;
+                break;
+            }
+            throw new AlteracaoStatusPedidoException();
+
+        case PAGO:
+            if (status.equals(Status.AGUARDANDO_CONFIRMACAO_PAGAMENTO)) {
+                status = newStatus;
+                break;
+            }
+            throw new AlteracaoStatusPedidoException();
+        
+        case PAGAMENTO_INVALIDO:
+        	if (status.equals(Status.AGUARDANDO_CONFIRMACAO_PAGAMENTO)) {
+        		status = newStatus;
+        		break;
+        	}
+        	throw new AlteracaoStatusPedidoException();
+
+        case EM_PREPARACAO:
+            if (status.equals(Status.PAGO)) {
+                status = newStatus;
+                break;
+            }
+            throw new AlteracaoStatusPedidoException();
+
+        case PRONTO:
+            if (status.equals(Status.EM_PREPARACAO)) {
+                status = newStatus;
+                dataConclusao = LocalDate.now();
+                break;
+            }
+            throw new AlteracaoStatusPedidoException();
+
+        case FINALIZADO:
+            if (status.equals(Status.PRONTO)) {
+                status = newStatus;
+                break;
+            }
+            throw new AlteracaoStatusPedidoException();	
+        }
 	}
 	
     public Status getStatus() {
