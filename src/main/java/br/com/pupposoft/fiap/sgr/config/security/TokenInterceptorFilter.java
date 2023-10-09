5eb1777ca6090fbf58231b732b9e113c12246cdc
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-10-09 19:52:29 -0300

    Configure spring security

--- src/main/java/br/com/pupposoft/fiap/sgr/config/security/TokenInterceptorFilter.java
+++ src/main/java/br/com/pupposoft/fiap/sgr/config/security/TokenInterceptorFilter.java
@@ -1,36 +1,34 @@
 package br.com.pupposoft.fiap.sgr.config.security;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import br.com.pupposoft.fiap.starter.token.TokenGateway;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@AllArgsConstructor
 public class TokenInterceptorFilter extends OncePerRequestFilter {
 
     private static final String TOKEN_PREFIX = "Bearer ";
-    private static final String USUARIO_ID = "usuario-id";
-    private static final String USUARIO_PERFIL = "usuario-perfil";
+    private static final String USUARIO_ID = "usuarioId";
+    private static final String USUARIO_PERFIL = "usuarioPerfil";
     
-    @Autowired
     private TokenGateway tokenGateway;
 
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -56,9 +54,7 @@ public class TokenInterceptorFilter extends OncePerRequestFilter {
 
 				addTokenInfosIntoHeader((HeaderMapRequestWrapper) requestWrapper, headersInfos);
 				
-				Collection<? extends GrantedAuthority> authorities = null;
-                Authentication auth = new UsernamePasswordAuthenticationToken(token, token, authorities);
-                SecurityContextHolder.getContext().setAuthentication(auth);
+				createAutentication(headersInfos);
 			}
 
 		} catch (Exception e) {
@@ -69,6 +65,13 @@ public class TokenInterceptorFilter extends OncePerRequestFilter {
 			log.trace("End requestPath={}", request.getServletPath());
 		}
 		
+	}
+
+	private void createAutentication(final Map<String, String> headersInfos) {
+		String role = headersInfos.get(USUARIO_PERFIL);
+		Collection<? extends GrantedAuthority> authorities = role == null ? null : Arrays.asList(() -> "ROLE_"+role);
+		Authentication auth = new UsernamePasswordAuthenticationToken("admin", "", authorities);
+		SecurityContextHolder.getContext().setAuthentication(auth);
 	}
 	
 	private Boolean isValidToken(String token) {
